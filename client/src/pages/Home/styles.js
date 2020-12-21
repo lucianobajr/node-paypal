b32830fe759bec6b9db9d484c5e02cd3bc5e0a7a
lucianobajr <luciano.alcantara@ufv.br>
2020-12-20 23:16:59 -0300

    add changes

--- client/src/pages/Home/styles.js
+++ client/src/pages/Home/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { darken } from 'polished'
 
 export const Wrapper = styled.div `
-  height: 100%;
+    height: 100%;
     display:-webkit-box;
     display:-ms-flexbox;
     display:flex;
@@ -16,7 +16,7 @@ export const Wrapper = styled.div `
 
 export const Content = styled.div `
     width:1366px;
-    height: 100%;
+    height: 3200px;
 
     display:flex;
     flex-direction:column;
@@ -24,9 +24,14 @@ export const Content = styled.div `
 
 export const Header = styled.header `
     margin-top:50px;
+
     .logo-box{
         display:flex;
         align-items:center;
+
+        @media (max-width:1300px) {
+            margin-left:40px;
+        }
 
         #title{
             color:#000;
@@ -47,6 +52,9 @@ export const Header = styled.header `
 `;
 
 export const Main = styled.section `
+    width:100%;
+    height: 2500px;
+
     margin-top:100px;
 
     .fs-content{
@@ -100,8 +108,162 @@ export const Main = styled.section `
             }
         }
     }
+
+    .sec-content{
+        width:1140px;
+    
+        display:flex;
+        align-items:center;
+        justify-content:space-between;
+
+        margin-top:100px;
+        margin-left:80px;
+
+        .boxes{
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            width:370px;
+            height: 100px;
+
+            
+
+            .icons-box{
+                border-radius:50%;
+                width:55px;
+                height:55px;
+
+                background-color:#FFECEC;
+
+                display:flex;
+                align-items:center;
+                justify-content:center;
+
+                transition: all .3s ease-in-out 0s;
+
+                margin-right:25px;
+
+                cursor: pointer;
+
+                &:hover{
+                    background-color:${darken(0.05,'#FFECEC')};
+                }
+            }
+
+            .texts{
+                .value{
+                    margin-bottom:15px;
+
+                    font-weight:bold;
+                    font-size:20px;
+                    font-family:GothamPro;
+                }
+                .type{
+                    font-family:GothamProRegular;
+                    font-size:17px;
+                    
+
+                    color:#292929;
+                }
+            }
+        }
+
+        .margin-box{
+            border-right:solid 2px #eee;
+        }
+    }
+
+    .third-content{
+        height: 500px;
+        width:100%;
+
+        margin-top:50px;
+        
+        background: rgb(244,244,244);
+        background: linear-gradient(180deg, rgba(244,244,244,1) 0%, rgba(255,255,255,1) 77%);
+
+        display:flex;
+        justify-content:center;
+        padding-top:70px;
+
+        .third-box{
+            width:350px;
+
+            margin-left:100px;
+            margin-top:50px;
+
+            display:flex;
+            flex-direction:column;
+            align-items:flex-start;
+
+            #title{
+                width:300px;
+
+                font-family:GothamPro;  
+                font-weight:bold;
+                font-size:26px;
+                line-height:1.5;
+            }
+            #subtitle{
+                margin-top:10px;
+                
+                font-family:GothamProRegular; 
+                line-height:2;
+                font-size:14px;
+            }
+
+            .list{
+
+                margin-top:20px;
+                li{
+                    display:flex;
+                    align-items:flex-end;
+
+                    margin-bottom:20px;
+                    .box-check{
+                        border-radius:50%;
+                        height: 24px;
+                        width:24px;
+                        background-color:#2FAB73;
+
+                        display:flex;
+                        align-items:center;
+                        justify-content:center;
+
+                        margin-right:10px;
+                    }
+
+                    p{
+                        font-family:GothamProRegular; 
+                        font-size:16px;
+                    }
+                }
+            }
+        }
+    }
+
+    .four-content{
+        margin-top:150px;
+
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+
+        #title{
+            font-weight:bold;
+            font-size:35px;
+            text-align:center;
+            font-family:GothamPro;
+            margin-bottom:100px;
+        }
+    }
 `;
 
-export const Footer = styled.footer `
+export const Footer = styled.div `
+    height: 563px;
+    width:100%;
+
+    background: rgb(255,255,255);
+    background: linear-gradient(90deg, rgba(255,255,255,1) 8%, rgba(238,238,238,1) 49%, rgba(255,255,255,1) 92%);
 
 `;
